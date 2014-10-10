b3df91a521d7d7787f49e92baadde942bf7a2be9
Alex Zuniga <azuniga90@yahoo.com>
2014-10-10 23:17:27 +0000

    This is the initial commit. Set up a laravel project to host PopRocks and culture.

---
+++ app/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h2> Thundercats are go! </h2>
+
+
+
+@stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	@yield('topscript')
+</head>
+<body>
+<h1> Hello World. This is Pop Rocks and Culture! </h1> 
+@yield('content')
+
+
+</body>
+@yield('bottomscript')
+</html>
